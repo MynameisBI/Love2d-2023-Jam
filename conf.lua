73feb5ac0a0a921d117a931966bfeaf5c05b9811
TheFabulousP <10421100@student.vgu.edu.vn>
2023-02-18 12:06:05 +0700

    upload puzzle 1's pieces

--- conf.lua
+++ conf.lua
@@ -1,3 +1,6 @@
 function love.conf(t)
-	
+	t.window.width = 1200
+  t.window.height = 700
+  t.window.title = "Jigsaw Puzzler"
+  t.window.icon = "sprites/puzzle1-answer.png"
 end
--- main.lua
+++ main.lua
@@ -1,8 +1,15 @@
 require 'globals'
 
+puzzle1 = {
+  7,
+  "puzzle1"
+}
 
 function love.load()
-
+  print("yay")
+  print(puzzle1[0])
+  -- for i in range(1, puzzle1[0] + 1) do
+  --   print(i)
 end
 
 function love.update(dt)
@@ -10,5 +17,4 @@ function love.update(dt)
 end
 
 function love.draw()
-
 end
