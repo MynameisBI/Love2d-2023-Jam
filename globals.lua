0b510ac181ed73ce115251c44ba58a3f0749d7d2
Rumis <20.21.9.12.1bibi@gmail.com>
2023-02-20 22:33:15 +0700

    Add levels frame to menu

--- globals.lua
+++ globals.lua
@@ -1,3 +1,4 @@
 Class = require 'libs.middleclass'
 Gamestate = require 'libs.hump.gamestate'
 Timer = require 'libs.hump.timer'
+Camera = require 'libs.hump.camera'
--- src/menu/menu.lua
+++ src/menu/menu.lua
@@ -6,12 +6,16 @@ local screenWidth, screenHeight = love.graphics.getDimensions()
 
 function Menu:enter()
   self.timer = Timer.new()
+  self.camera = Camera.new()
+  self.camera.smooth = Camera.smooth.damped(4.8)
 
   self.smui = Smui()
+  self.currentFrame = 'main'
 
-  self.startButton = self.smui:Button(screenWidth/2 - 140, 220, 280, 70, 'wassup baby girl', Fonts.menu_medium)
+  -- Main frame
+  self.startButton = self.smui:Button(screenWidth/2 - 140, 220, 280, 70, 'PLAY', Fonts.menu_medium)
   self.startButton.released = function(startButton, x, y, button)
-    Gamestate.switch(Game)
+    self.currentFrame = 'levels'
   end
   self.startButton.enter = function(startButton)
     if self.startButton.tweenHandle ~= nil then 
@@ -23,27 +27,56 @@ function Menu:enter()
     self.timer:cancel(self.startButton.tweenHandle)
     self.startButton.tweenHandle = self.timer:tween(0.4, startButton, {sx = 1, sy = 1, x = screenWidth/2 - 140, y = 220}, 'out-quint')
   end
+
+  -- Levels frame
+  self.level1 = self.smui:Button(1400, 200, 200, 200)
+  self.level1.released = function()
+    Gamestate.switch(Game)
+  end
+  self.level2 = self.smui:Button(1700, 200, 200, 200)
+  self.level2.released = function()
+    Gamestate.switch(Game)
+  end
+  self.level3 = self.smui:Button(2000, 200, 200, 200)
+  self.level3.released = function()
+    Gamestate.switch(Game)
+  end
+  self.levelBack = self.smui:Button(2200, 600, 140, 60, 'BACK', Fonts.menu_medium)
+  self.levelBack.released = function(levelBack, x, y, button)
+    self.currentFrame = 'main'
+  end
 end
 
 function Menu:update(dt)
   self.timer:update(dt)
   self.smui:update(dt)
+
+  if self.currentFrame == 'main' then
+    self.camera:lockX(screenWidth * 0.5, self.camera.smooth)
+  elseif self.currentFrame == 'levels' then
+    self.camera:lockX(screenWidth * 1.5, self.camera.smooth)
+  end
 end
 
 function Menu:draw()
-  self.smui:draw()
+  self.camera:attach()
+    self.smui:draw()    
+  self.camera:detach()
 end
 
 function Menu:mousepressed(x, y, button)
-  self.smui:mousepressed(x, y, button)
+  local mx, my = self.camera:mousePosition()
+  self.smui:mousepressed(mx, my, button)
 end
 
 function Menu:mousereleased(x, y, button)
-  self.smui:mousereleased(x, y, button)
+  local mx, my = self.camera:mousePosition()
+  self.smui:mousereleased(mx, my, button)
 end
 
 function Menu:mousemoved(x, y)
-  self.smui:mousemoved(x, y)
+  local mx, my = self.camera:mousePosition()
+  self.smui:mousemoved(mx, my)
 end
 
 return Menu
